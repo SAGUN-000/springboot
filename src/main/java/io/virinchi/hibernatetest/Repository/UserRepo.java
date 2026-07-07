8b9d7607efe4696c449bd4bcbc74edbed2d60999
SAGUN 000 <sagunbista4321@gmail.com>
2026-07-07 12:37:14 +0545

    Remove old Hibernate project

--- src/main/java/io/virinchi/hibernatetest/Repository/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package io.virinchi.hibernatetest.Repository;
-
-import io.virinchi.hibernatetest.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
